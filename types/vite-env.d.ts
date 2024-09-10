db3cc3bd96af86f9666c5154c70c4597b7616580
岳晓亮 <2220124666@qq.com>
2024-09-10 10:32:49 +0800

    fix: 修复 ts 类型报错

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_GITHUB_AUTH: string
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
