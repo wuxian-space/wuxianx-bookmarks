62d430459bcbb136a10d30d3286cb72d7f3d6f91
岳晓亮 <2220124666@qq.com>
2024-09-19 10:03:41 +0800

    refactor(hooks/useCssVariables): 重构代码，使代码编写更简洁

--- src/hooks/useCssVariables.ts
+++ src/hooks/useCssVariables.ts
@@ -1,7 +1,7 @@
 import { useCssVar } from '@vueuse/core';
 import { watch } from 'vue';
 import { kebabCase } from 'lodash-es'
-import useThemeConfig, { ThemeConfig } from '@/stores/useThemeConfig'
+import useThemeConfig, { type ThemeConfig, baseCssVariables, asideCssVariables, headerCssVariables, linkCssVariables } from '@/stores/useThemeConfig'
 import { NAMESPACE } from '@/constants'
 
 const root = document.documentElement
@@ -11,26 +11,21 @@ export function setLayoutCssVars() {
 
   return {
     setLayout() {
-      sv('bgColor', root)
-      sv('textColor', root);
-      sv('fontFamily', root);
-      sv('fontSize', root);
-      sv('borderColor', root);
-
-      sv('asideWidth', root, (val) => `${val}px`)
-      sv('asideBgColor', root);
-      sv('asideTextColor', root);
-      sv('asideItemColor', root)
-      sv('asideItemMinHeight', root, (val) => `${val}px`)
-      sv('asideActiveItemColor', root);
-
-      sv('headerHeight', root, (val) => `${val}px`)
-      sv('headerBgColor', root);
-      sv('headerTextColor', root);
-      sv('headerTitleFontSize', root, (val) => `${val}px`)
-
+      svGroup(baseCssVariables)
+      svGroup(linkCssVariables)
+      svGroup(headerCssVariables)
+      svGroup(asideCssVariables)
       return root
     },
+  }
+
+  function svGroup(data: Record<string, any>, el: any = root) {
+    Object.keys(data).forEach((key) => {
+      sv(key as any, el, (val) => {
+        if (typeof val === 'number') return `${val}px`
+        return val
+      })
+    })
   }
 
   function sv(key: keyof ThemeConfig, el: any, handler?: (val: any) => string) {
